5d57ae492f18b00788da6ee4e8c21707fe97a985
Shannon Williams <swilliams@stingraymedia.com>
2026-03-18 20:54:08 -0500

    Update X and YouTube social media profile URLs

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -10,7 +10,7 @@ const socials = [
   },
   {
     name: "X",
-    url: "https://x.com/tcj345",
+    url: "https://x.com/TCJ345_",
     icon: (
       <svg viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
         <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
@@ -28,7 +28,7 @@ const socials = [
   },
   {
     name: "YouTube",
-    url: "https://www.youtube.com/@CaymanianJournal",
+    url: "https://www.youtube.com/@TheCaymanianJournal",
     icon: (
       <svg viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
         <path d="M23.498 6.186a3.016 3.016 0 00-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 00.502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 002.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 002.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
